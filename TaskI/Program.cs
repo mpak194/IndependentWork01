66d10f23b4a65cd7df4d44bffede3495e740a811
mpak194 <55350529+mpak194@users.noreply.github.com>
2019-09-23 00:12:42 +0300

    Add files via upload

---
+++ TaskI/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+
+namespace TaskI
+{
+    class Program
+    {
+
+
+        /// <summary>
+        /// Метод считывания целочисленных переменных
+        /// </summary>
+        /// <param name="inputString">Строка, которую необходимо преобразовать в int</param>
+        /// <param name="checkValue">Индикатор успешного преобразования</param>
+        /// <returns>Целочисленное значение int</returns>
+        static int InputIntValue(string inputString, out bool checkValue)
+        {
+            int number;
+            if (int.TryParse(inputString, out number))
+            {
+                checkValue = true;
+                return number;
+            }
+            checkValue = false;
+            return 0;
+        }
+
+
+
+        /// <summary>
+        /// Метод определения точной степени двойки
+        /// </summary>
+        /// <param name="number"></param>
+        static void IsDegreeTwo(int number)
+        {
+            // Индикатор поиска степени двойки.
+            bool isDegree = false;
+
+            for (long i = 1; i <= number; i *= 2)
+            {
+                if (i == number)
+                {
+                    isDegree = true;
+                    break;
+                }
+                
+            }
+
+            // Вывод результата.
+            if (isDegree) Console.WriteLine("YES");
+            else Console.WriteLine("NO");
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            bool checkNumber;
+
+
+            // Ввод значений.
+            int number = InputIntValue(Console.ReadLine(), out checkNumber);
+
+            // Проверка значений.
+            if (checkNumber)
+            {
+                // Проверка числа на степень двойки и вывод результата.
+                IsDegreeTwo(number);
+            }
+            // Вывод ошибки.
+            else Console.WriteLine("wrong");
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TaskJ/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+
+namespace TaskJ
+{
+    class Program
+    {
+
+
+        /// <summary>
+        /// Метод считывания целочисленных переменных
+        /// </summary>
+        /// <param name="inputString">Строка, которую необходимо преобразовать в int</param>
+        /// <param name="checkValue">Индикатор успешного преобразования</param>
+        /// <returns>Целочисленное значение int</returns>
+        static int InputIntValue(string inputString, out bool checkValue)
+        {
+            int number;
+            if (int.TryParse(inputString, out number))
+            {
+                checkValue = true;
+                return number;
+            }
+            checkValue = false;
+            return 0;
+        }
+
+
+
+        /// <summary>
+        /// Метод определения номера числа Фибоначчи
+        /// </summary>
+        /// <param name="number">Проверяемое число</param>
+        static void GetFibNumber (int number)
+        {
+            // Индикатор определения числа Фибоначчи.
+            bool isFib = false;
+
+            // Массив, содержащий ряд Фибоначчи.
+            int[] FibSeq = new int[10000];
+            FibSeq[0] = 0;
+            FibSeq[1] = 1;
+
+            // Порядковый номер числа Фибоначчи.
+            int fibElement;
+
+            // Проверка числа с рядом Фибоначчи.
+            for (fibElement = 2; ; fibElement++)
+            {
+                // Формула текущего элемента.
+                FibSeq[fibElement] = FibSeq[fibElement - 1] + FibSeq[fibElement - 2];
+
+
+                if (number == FibSeq[fibElement])
+                {
+                    isFib = true;
+                    break;
+                }
+
+
+                if (number < FibSeq[fibElement])
+                {
+                    isFib = false;
+                    break;
+                }
+            }
+
+            // Вывод результата.
+            if (isFib) Console.WriteLine(fibElement);
+            else Console.WriteLine("-1");
+        }
+
+
+        static void Main(string[] args)
+        {
+            bool checkNumber;
+
+
+            // Ввод значений.
+            int number = InputIntValue(Console.ReadLine(), out checkNumber);
+
+            // Проверка значений.
+            if (checkNumber && number > 1)
+            {
+                // Определение номера числа Фибоначчи.
+                GetFibNumber(number);
+
+
+            }
+            // Вывод ошибки.
+            else Console.WriteLine("wrong");
+
+
+            Console.ReadLine();
+        }
+    }
+}
